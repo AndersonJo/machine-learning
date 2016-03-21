bc86b5da72bbe21f20c6495fdc3b8b5100a45cbe
Anderson Jo <a141890@gmail.com>
2016-03-21 10:27:25 +0900

    Remove useless things

--- amanda-system-information/awsfab_settings.py
+++
@@ -1,6 +0,0 @@
-AUTH = {'aws_access_key_id': 'AKIAICAM2JVSUUKXHP2Q',
-        'aws_secret_access_key': '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'}
-
-DEFAULT_REGION = 'ap-northeast-1'
-KEYPAIR_PATH = '~/.ssh'
-EC2_INSTANCE_DEFAULT_SSHUSER = 'ubuntu'
