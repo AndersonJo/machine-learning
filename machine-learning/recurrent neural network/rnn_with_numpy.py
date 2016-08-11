4199053a7fa2c53505704a5f8e4d547c473d38db
Anderson <a141890@gmail.com>
2016-08-12 00:12:05 +0900

    Improve RNN

--- machine-learning/recurrent neural network/rnn_with_numpy.py
+++ machine-learning/recurrent neural network/rnn_with_numpy.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-class RNNNumpy(object):
 
+class RNNNumpy(object):
     def __init__(self, word_dim, hidden_dim=100):
         self.word_dim = word_dim
         self.hidden_dim = hidden_dim
-        self.U = np.random.uniform(-1./-np.sqrt(word_dim), 1./np.sqrt(word_dim), (hidden_dim, word_dim))
-        self.V = np.random.uniform(-1. / -np.sqrt(word_dim), 1. / np.sqrt(word_dim), (word_dim, hidden_dim))
-        self.W = np.random.uniform(-1. / -np.sqrt(word_dim), 1. / np.sqrt(word_dim), (hidden_dim, word_dim))
+        self.U = np.random.uniform(-1. / np.sqrt(word_dim), 1. / np.sqrt(word_dim), (hidden_dim, word_dim))
+        self.V = np.random.uniform(-1. / np.sqrt(word_dim), 1. / np.sqrt(word_dim), (word_dim, hidden_dim))
+        self.W = np.random.uniform(-1. / np.sqrt(word_dim), 1. / np.sqrt(word_dim), (hidden_dim, word_dim))
 
     def forward_propagation(self, x):
         # The total number of time steps
@@ -20,14 +20,14 @@ class RNNNumpy(object):
         # The outputs at each time step. Again, we save them for later.
         o = np.zeros((T, self.word_dim))
         for t in xrange(T):
-            s[t] = np.tanh(self.U[:, x[t]] + self.W.dot(s[t-1]))
-            o[t] = softmax(self.V.dot(s[t]))
+            s[t] = self.U[:, x[t]]
+            o[t] = self.softmax(self.V.dot(s[t]))
         return [o, s]
+
+    def softmax(self, v, t=1.0):
+        e = np.exp(v / t)
+        return e/np.sum(e)
 
     def predict(self, x):
         o, x = self.forward_propagation(x)
         return np.argmax(o, axis=1)
-
-
-
-
