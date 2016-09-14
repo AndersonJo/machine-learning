29d27b53042d32b0fe2c59456e66b3a2df890607
Anderson <a141890@gmail.com>
2016-09-14 21:56:44 +0900

    asdf

--- machine_learning_tutorial/RNN Predicting the next word via Python/rnn_with_numpy.py
+++ machine_learning_tutorial/RNN Predicting the next word via Python/rnn_with_numpy.py
@@ -1,4 +1,7 @@
 # -*- coding:utf-8 -*-
+from datetime import datetime
+from random import randint
+
 import numpy as np
 
 
@@ -11,9 +14,6 @@ class RNNNumpy(object):
         self.U = np.random.uniform(-1. / np.sqrt(word_dim), 1. / np.sqrt(word_dim), (hidden_dim, word_dim))
         self.V = np.random.uniform(-1. / np.sqrt(word_dim), 1. / np.sqrt(word_dim), (word_dim, hidden_dim))
         self.W = np.random.uniform(-1. / np.sqrt(word_dim), 1. / np.sqrt(word_dim), (hidden_dim, hidden_dim))
-        print self.U.shape
-        print self.V.shape
-        print self.W.shape
 
     def forward_propagation(self, x):
         """
@@ -31,7 +31,6 @@ class RNNNumpy(object):
         o = np.zeros((T, self.word_dim))
 
         for t in xrange(T):
-
             s[t] = np.tanh(self.U[:, x[t]] + self.W.dot(s[t - 1]))
             o[t] = self.softmax(self.V.dot(s[t]))
 
@@ -39,15 +38,12 @@ class RNNNumpy(object):
 
     def cross_entropy(self, x, y):
         """
-        :param x: 문장들이 여러개 들어있는 matrix. 즉 training에 사용되는 전체 matrix
+        :param x: sentence
         """
-
-        N = np.sum((len(sentence) for sentence in y))
-        L = 0
-        for i in xrange(len(y)):
-            o, s = self.forward_propagation(x[i])
-            predicted_output = o[np.arange(len(y[i])), y[i]]
-            L += np.sum(np.log(predicted_output))
+        N = len(y)
+        o, s = self.forward_propagation(x)
+        predicted_output = o[np.arange(len(y)), y]
+        L = np.sum(np.log(predicted_output))
         return -1 * L / N
 
     def bptt(self, x, y):
@@ -64,79 +60,41 @@ class RNNNumpy(object):
 
         for t in np.arange(T)[::-1]:
             dldV += np.outer(delta_o[t], s[t].T)
-            print 'dldV', dldV
             # Initial delta calculation
             delta_t = self.V.T.dot(delta_o[t]) * (1 - (s[t] ** 2))
 
-            # Backpropagration through time (for at most self.bptt_truncate steps)
             for bptt_step in np.arange(max(0, t - self.bptt_truncate), t + 1)[::-1]:
                 dldW += np.outer(delta_t, s[bptt_step - 1])
-                dldU[: x[bptt_step]] += delta_t
+                dldU[:, x[bptt_step]] += delta_t
                 delta_t = self.W.T.dot(delta_t) * (1 - s[bptt_step - 1] ** 2)
         return dldU, dldV, dldW
 
-    def gradient_check(self, x, y, h=0.001, error_threshold=0.01):
-        # Calculate the gradients using backpropagation. We want to checker if these are correct.
-        bptt_gradients = self.bptt(x, y)
-        # List of all parameters we want to check.
-        model_parameters = ['U', 'V', 'W']
-        # Gradient check for each parameter
-        for pidx, pname in enumerate(model_parameters):
-            # Get the actual parameter value from the mode, e.g. model.W
-            parameter = operator.attrgetter(pname)(self)
-            print "Performing gradient check for parameter %s with size %d." % (pname, np.prod(parameter.shape))
-            # Iterate over each element of the parameter matrix, e.g. (0,0), (0,1), ...
-            it = np.nditer(parameter, flags=['multi_index'], op_flags=['readwrite'])
-            while not it.finished:
-                ix = it.multi_index
-                # Save the original value so we can reset it later
-                original_value = parameter[ix]
-                # Estimate the gradient using (f(x+h) - f(x-h))/(2*h)
-                parameter[ix] = original_value + h
-                gradplus = self.calculate_total_loss([x], [y])
-                parameter[ix] = original_value - h
-                gradminus = self.calculate_total_loss([x], [y])
-                estimated_gradient = (gradplus - gradminus) / (2 * h)
-                # Reset parameter to original value
-                parameter[ix] = original_value
-                # The gradient for this parameter calculated using backpropagation
-                backprop_gradient = bptt_gradients[pidx][ix]
-                # calculate The relative error: (|x - y|/(|x| + |y|))
-                relative_error = np.abs(backprop_gradient - estimated_gradient) / (
-                    np.abs(backprop_gradient) + np.abs(estimated_gradient))
-                # If the error is to large fail the gradient check
-                if relative_error and gt and error_threshold:
-                    print "Gradient Check ERROR: parameter=%s ix=%s" % (pname, ix)
-                    print "+h Loss: %f" % gradplus
-                    print "-h Loss: %f" % gradminus
-                    print "Estimated_gradient: %f" % estimated_gradient
-                    print "Backpropagation gradient: %f" % backprop_gradient
-                    print "Relative Error: %f" % relative_error
-                    return
-                it.iternext()
-            print "Gradient check for parameter %s passed." % (pname)
+    def calculate_gradients(self, x, y, learning_rate=0.005):
+        # Calculate the gradients
+        dLdU, dLdV, dLdW = self.bptt(x, y)
+        # Change parameters according to gradients and learning rate
+        self.U -= learning_rate * dLdU
+        self.V -= learning_rate * dLdV
+        self.W -= learning_rate * dLdW
 
-    def train_with_sgd(model, X_train, y_train, learning_rate=0.005, nepoch=100, evaluate_loss_after=5):
-        # We keep track of the losses so we can plot them later
-        losses = []
-        num_examples_seen = 0
-        for epoch in range(nepoch):
-            # Optionally evaluate the loss
-            if epoch % evaluate_loss_after == 0:
-                loss = model.calculate_loss(X_train, y_train)
-                losses.append((num_examples_seen, loss))
-                time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                print "%s: Loss after num_examples_seen=%d epoch=%d: %f" % (time, num_examples_seen, epoch, loss)
-                # Adjust the learning rate if loss increases
-                if len(losses) > 1 and losses[-1][1] > losses[-2][1]:
-                    learning_rate *= 0.5
-                    print "Setting learning rate to %f" % learning_rate
-                sys.stdout.flush()
-            # For each training example...
-            for i in range(len(y_train)):
-                # One SGD step
-                model.sgd_step(X_train[i], y_train[i], learning_rate)
-                num_examples_seen += 1
+    def train(self, x_train, y_train, learning_rate=0.005, npoch=100):
+        N = len(y_train)
+        loss_show = N / 10
+
+        print 'Start Training'
+        print 'Total Data: ', N
+
+        for i in xrange(npoch):
+            # One SGD step
+            rand_idx = randint(0, N)
+            self.calculate_gradients(x_train[rand_idx], y_train[rand_idx], learning_rate)
+            if i % 100 == 0:
+                self._print_error(self.cross_entropy(x_train[i], y_train[i]), i)
+
+    @staticmethod
+    def _print_error(cost, i):
+        t = datetime.now()
+        print '{time}: i={i}, cost={cost}'.format(time=t, i=i, cost=cost)
 
     def softmax(self, v, t=1.0):
         e = np.exp(v / t)
